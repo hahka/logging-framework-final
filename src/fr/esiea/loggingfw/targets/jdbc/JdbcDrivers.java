9aaf4099a5221b97c02d3553ff8b85452090363a
Thibaut Virolle <thibaut.virolle@gmail.com>
2015-02-21 12:22:38 +0100

    Ajout des drivers jdbc pour mysql et postgresql

---
+++ src/fr/esiea/loggingfw/targets/jdbc/JdbcDrivers.java
@@ -0,0 +1,8 @@
+package fr.esiea.loggingfw.targets.jdbc;
+
+public class JdbcDrivers {
+
+	public final String POSTGRESQL_DRIVER = "org.postgresql.Driver";
+	public final String MYSQL_DRIVER = "com.mysql.jdbc.Driver";
+
+}
